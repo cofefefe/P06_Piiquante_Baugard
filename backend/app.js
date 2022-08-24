adac6f363df490ea2b111ded98de64a62e8e0459
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-08-24 16:24:27 +0200

    feat:need fix CORS

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    )
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    )
+    next()
+  })
+
+
+app.use(express.json())
+
 const uri = 'mongodb+srv://cofefefe:Trinite333@cluster0.hzpfkea.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(uri, {useNewUrlParser:true})
-.then(()=> res.status(201).console.log('connexion réussie à la base de donnée'))
-.catch((error)=> console.log({error}))
+mongoose.connect(uri,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use('/api/auth', userRoutes);
+
+app.use(bodyParser.json())
+
 
 module.exports = app
