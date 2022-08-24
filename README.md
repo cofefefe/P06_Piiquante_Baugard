80a8036bd4ba7f243ca4938ac5097b38fbaa850e
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-08-24 11:25:25 +0200

    feat:initialzing local server for back and frontend

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # P06_Piiquante_Baugard
 Openclassroom's project | working on node, express, mongoDB | backend
+
+# Getting started
+1. 'cd frontend'
+2. 'npm install' (if not already done)
+3. 'npm run start'
+4. go to test[http://localhost4200](http://localhost:4200)
+5. 'cd ../backend'
+7. 'npm install' ( if not already done )
+8. 'npm run start'
--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const uri = 'mongodb+srv://cofefefe:Trinite333@cluster0.hzpfkea.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTypology:true })
-    .then(()=> console.log('connexion réussie à la base de donnée'))
-    .catch(()=> console.log('connexion échouée'))
+// mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTypology:true })
+//     .then(()=> console.log('connexion réussie à la base de donnée'))
+//     .catch(()=> console.log('connexion échouée'))
 
 module.exports = app
