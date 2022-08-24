e66edba724f198348126fbad5995ef2195a71996
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-08-24 12:49:21 +0200

    feat:Initialzing login and signup for users

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+const userRoutes = require('./routes/user')
+
+const app = express()
 const uri = 'mongodb+srv://cofefefe:Trinite333@cluster0.hzpfkea.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(uri, {useNewUrlParser:true})
-    .then(()=> res.status(201).console.log('connexion réussie à la base de donnée'))
-    .catch((error)=> console.log({error}))
+.then(()=> res.status(201).console.log('connexion réussie à la base de donnée'))
+.catch((error)=> console.log({error}))
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app
