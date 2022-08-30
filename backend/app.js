0318044d61d0b6377e7c2bbe0cd2863ad6e76e64
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-08-30 13:44:52 +0200

    fix: server error

--- backend/app.js
+++ backend/app.js
@@ -8,19 +8,15 @@ const userRoutes = require('./routes/user')
 const app = express()
 
 // have access to request body
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(express.json());
 
 // Cors permission
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  )
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  ),
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Accept, Content-Type, Authorization")
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS"),
+  res.setHeader('responseType',  'text')
   next()
 })
 
@@ -33,9 +29,12 @@ mongoose.connect(uri,
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'))
 
-
-
+app.use((req,res,next)=>{
+  res.end('ok')
+  next()
+})
 app.post("/api/auth", userRoutes)
+
 
 
 module.exports = app
